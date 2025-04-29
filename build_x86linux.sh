090ab6e07d0d08aaac59623564c058cd10f8da55
lau_sun <1125872936@qq.com>
2025-04-29 16:02:35 +0800

    编译区分arm & x86 linux

---
+++ build_x86linux.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+LIVE555_DIR=`pwd`
+
+cd $LIVE555_DIR
+
+INSTALL_DIR=$LIVE555_DIR/output
+
+if [ "$1" == "clean" ]; then
+    echo "do make clean ..."
+    make clean
+    rm -rf "$INSTALL_DIR"
+    exit 0
+fi
+
+mkdir -p $INSTALL_DIR
+
+# 编译成静态库
+export LDFLAGS="-static"
+
+# 声明交叉编译器的路径
+# export PATH=/opt/arm-rockchip830-linux-uclibcgnueabihf/bin/:$PATH
+
+./genMakefiles linux
+
+
+# CROSS_COMPILE 可以根据平台不同进行修改
+make -j$(nproc)
+
+#make install PREFIX=$INSTALL_DIR CROSS_COMPILE=arm-rockchip830-linux-uclibcgnueabihf-
+
